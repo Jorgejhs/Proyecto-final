3d1f61fd3afed3fad2dbc48b748186912f7efa07
jorge <autsanchez@hotmail.com>
2022-12-10 16:06:42 -0300

    js al form

--- js/proy final.js
+++ js/proy final.js
@@ -10,5 +10,9 @@ instagram.addEventListener("click", ()=> {
 let facebook = document.getElementById("facebook");
 facebook.addEventListener("click", ()=> {
     alert ("Vas a salir del sitio web");});
+    
+let form = document.getElementById("enviar");
+form.addEventListener("click", ()=> {
+    alert ("Tu respuesta fué enviada con éxito");});
 
     
